a1817224df716462e100c5e2fa777f912babab05
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2019-02-03 22:56:41 +0800

    Update 004.py

--- DA1/004.py
+++ DA1/004.py
@@ -14,11 +14,14 @@ books = pd.read_excel('./Books.xlsx', skiprows=3, usecols='C:F', index_col=None,
     dtype={'ID':str,'InStore':str,'Date':str})
 start = date(2018,1,1)
 for i in books.index:
-    books['ID'].at[i] = i + 1
-    books['InStore'].at[i] = 'Yes' if i%2 == 0 else 'No'
+    # books['ID'].at[i] = i + 1
+    books.at[i, 'ID'] = i + 1
+    # books['InStore'].at[i] = 'Yes' if i%2 == 0 else 'No'
+    books.at[i, 'InStore'] = 'Yes' if i%2 == 0 else 'No'
     # books['Date'].at[i] = start + timedelta(days=i)
     # books['Date'].at[i] = date(start.year+i, start.month, start.day)
-    books['Date'].at[i] = add_month(start, i)
+    # books['Date'].at[i] = add_month(start, i)
+    books.at[i, 'Date'] = add_month(start, i)
 
 books.set_index('ID', inplace=True)
 print(books)
