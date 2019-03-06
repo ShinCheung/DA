fda92700ba6bbfc17e3e1d09bc153e96fee9e49f
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2019-03-06 14:59:20 +0800

    Update 018.py

--- DA4/018.py
+++ DA4/018.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 employees = pd.read_excel('./Employees.xlsx', index_col='ID')
+# 参数expand，这个参数取True时，会把切割出来的内容当做一列。 如果不需要pandas为你分好列，expand=False就可以了
 df = employees['Full Name'].str.split(expand=True)
 employees['FirstName'] = df[0]
 employees['LastName'] = df[1].str.upper()
